9c6a80ea4f061cf377b1b1dac4d48336c268b24b
Ahmad EbrahimZade <Ahmad_Ehz@yahoo.com>
2026-02-26 13:11:01 +0330

    Fix bug when some beans not found

--- src/main/java/io/openfeign/extensions/compositerequest/config/CompositeRequestConfiguration.java
+++ src/main/java/io/openfeign/extensions/compositerequest/config/CompositeRequestConfiguration.java
@@ -3,6 +3,7 @@ package io.openfeign.extensions.compositerequest.config;
 import feign.Contract;
 import io.openfeign.extensions.compositerequest.feign.CompositeRequestContract;
 import io.openfeign.extensions.compositerequest.feign.CompositeRequestInvocationHandlerFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.openfeign.AnnotatedParameterProcessor;
 import org.springframework.cloud.openfeign.FeignBuilderCustomizer;
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.FeignClientProperties;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.format.support.DefaultFormattingConversionService;
 
 import java.util.List;
 
@@ -18,9 +20,10 @@ public class CompositeRequestConfiguration {
 
     @Bean
     Contract compositeRequestContract(List<AnnotatedParameterProcessor> annotatedParameterProcessors,
-                                      @Qualifier("feignConversionService") ConversionService conversionService,
-                                      FeignClientProperties properties) {
-        SpringMvcContract springMvcContract = new SpringMvcContract(annotatedParameterProcessors, conversionService, properties);
+                                      @Qualifier("feignConversionService") ObjectProvider<ConversionService> conversionService,
+                                      ObjectProvider<FeignClientProperties> properties) {
+        ConversionService service = conversionService.getIfAvailable(DefaultFormattingConversionService::new);
+        SpringMvcContract springMvcContract = new SpringMvcContract(annotatedParameterProcessors, service, properties.getIfAvailable());
         return new CompositeRequestContract(springMvcContract);
     }
 
